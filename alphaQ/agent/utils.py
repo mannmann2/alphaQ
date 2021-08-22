e9d7e0dcace1f7ffdd52e8606f76798afa9be68c
arman <armanmann2@gmail.com>
2021-08-22 12:17:50 +0530

    Added function to display model params

--- alphaQ/agent/utils.py
+++ alphaQ/agent/utils.py
@@ -32,3 +32,31 @@ class Record:
         self.actions.iloc[0]['CASH'] = 1.0
         # record of episode summaries
         self.episodes = []
+
+
+def display_attributes(agent, obs_space=None):
+    """Display selected agent attributes."""
+    print('learning_rate:', agent.learning_rate)
+    print('gamma:', agent.gamma)
+    print('batch_size:', agent.batch_size)
+    print('buffer_size:', agent.buffer_size)
+    print('polyak_update:', agent.tau)
+
+    if agent.action_noise:
+        print('action_noise', agent.action_noise)
+
+    eps = agent.__dict__.get('exploration_initial_eps')
+    if eps:
+        print('epsilon_initial:', eps,
+              '\tepislon_final:', agent.exploration_final_eps,
+              '\tepislon_fraction:', agent.exploration_fraction)
+
+    if 'features_extractor_class' in agent.policy_kwargs:
+        x = agent.policy_kwargs['features_extractor_class'](obs_space)
+        print('feature_extractor:')
+        print(x.cnn)
+
+    if 'net_arch' in agent.policy_kwargs:
+        print('net_arch:', agent.policy_kwargs['net_arch'])
+
+    print()
